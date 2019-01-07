f2154641d47cdde4807a9f87913222e9434c8ef2
wilsonwu0208 <gx7879@gmail.com>
2019-01-07 11:17:16 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  created(){
+    const api = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/products`;
+    this.$http.get(api).then((response)=>{
+      console.log(response)
+    })
+  }
+}
+</script>
+
+<style lang="sass">
+@import "~bootstrap/scss/bootstrap"
+#nav
+  padding: 30px
+  a
+    font-weight: bold
+    color: #2c3e50
+    &.router-link-exact-active
+      color: #42b983
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <a href="#" @click.prevent="signout">登出</a>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    signout(){
+      const api = `${process.env.VUE_APP_APIPATH}/logout`;
+      const vm = this;
+      this.$http.post(api).then((response)=>{
+        console.log(response.data)
+        if(response.data.success){
+          vm.$router.push('/login');
+        }
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
