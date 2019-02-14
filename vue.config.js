adf67ee87727f592425e1abc9cb2cda20ad2a43a
wilsonwu0208 <gx7879@gmail.com>
2019-02-14 15:53:24 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  baseUrl: '/dist/'
+  baseUrl: '/vue-dashboard-demo/dist/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
